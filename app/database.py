c44de68da092819fc2e47f814936648282ed5a21
Inbar <Inbark2010@gmail.com>
2025-05-05 12:24:03 +0300

    commit

--- app/database.py
+++ app/database.py
@@ -1,13 +1,16 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
 
+load_dotenv()
 
-SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://root:22&2011@localhost:3306/reading_list_manager_schema"
+SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
+
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
-# Dependency for database sessions
 def get_db():
     db = SessionLocal()
     try:
